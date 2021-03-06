dbf0d3710ff174146ec2039eea6c05e870123e8b
A01114713 <A01114713@itesm.mx>
2021-03-06 00:09:11 +0000

    setup

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int numarg, char **argv){ //Main puede recibir dos argumentos, numarg que dicta el numero de argumentos
+	for(int i = 1; i < numarg; i++){
+		printf("Hello world %s\n",argv[i]);	
+	}
+	return 0;
+}
