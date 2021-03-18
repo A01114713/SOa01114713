9d911babff0c0ffd5da9e0983a861b9aaeb9161d
A01114713 <A01114713@itesm.mx>
2021-03-18 04:23:38 +0000

    laboratorio3

---
+++ IPC/signal.c
@@ -0,0 +1,25 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h> //libreria para sleep
+#include <stdbool.h>
+
+bool signalBool = false;
+
+void signalHandler(int sig){
+	printf("Recibi segnal %d\n", sig);
+	signalBool = true;
+}
+
+int main(){
+	signal(2, signalHandler); //cuando recibas una señal 15, ejecuta la funcion signalHandler
+	signal(15, signalHandler);
+	while(1){
+		if(signalBool){
+			break;
+		}
+		printf("Trabajando\n");
+		sleep(1);
+	}
+	printf("Ya voy a terminar\n");
+	return 0;
+}
